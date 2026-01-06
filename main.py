1589b586d764734a4e2888b094f4e3aef75a8841
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-01-06 16:40:53 -0500

    ch2l3

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text(path: str = "")->str:
+    with open(path) as f:
+        file_contents: str = f.read()
+    return file_contents
+
+def main():
+    print(
+        get_book_text(
+            path = "books/frankenstein.txt"
+        )
+    )
+
+if __name__ == "__main__":
+    main()
