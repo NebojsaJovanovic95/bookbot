dab732d71c6440ad2ad0a2ec301e83d2288385d6
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-01-06 16:48:21 -0500

    ch2l4

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ def get_book_text(path: str = "")->str:
         file_contents: str = f.read()
     return file_contents
 
-def main():
-    print(
-        get_book_text(
-            path = "books/frankenstein.txt"
-        )
+def count_words(text:str = "") -> int:
+    return len(
+        text.split()
     )
+
+def main():
+    book_text: str = get_book_text(
+        path = "books/frankenstein.txt"
+    )
+    num_words:int = count_words(text = book_text)
+    print(f"Found {num_words} total words")
 
 if __name__ == "__main__":
     main()
